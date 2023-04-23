8e8b9b6d353619e6e43435a1f3ebd380d04c3c18
Evgenij-svg <66062085+Evgenij-svg@users.noreply.github.com>
2023-04-23 20:21:18 +0300

    Revert "Clean code s1e1"

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -70,7 +70,7 @@ var addTask=function(){
     bindTaskEvents(listItem, taskCompleted);
 
     taskInput.value="";
-    
+
 }
 
 //Edit an existing task.
